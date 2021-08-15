6a8c8c2c64ffae0beefef975b30ae25c80dc238f
3ndymi0n <endymi0n@systemdecay.info>
2021-08-15 20:58:56 +1000

    Added contentCard function

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ document.querySelector("#add-title").addEventListener("submit", (e) => {
   try {
     const newTitleFormData = new FormData(e.target);
     let newTitle = Object.fromEntries(newTitleFormData.entries());
-
     library.addBook(
       newTitle.title,
       newTitle.author,
@@ -21,6 +20,20 @@ document.querySelector("#add-title").addEventListener("submit", (e) => {
 
 const library = (function () {
   let bookCollection = {};
+  
+  checkLocalStorage();
+
+  function checkLocalStorage() {
+      if(localStorage.getItem(bookCollection)){
+          bookCollection = JSON.parse(localStorage.getItem('bookCollection'));
+      }else {
+          bookCollection = {};
+      };
+  }
+
+  function updateLocalStorage() {
+    localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
+  }
 
   // generateId helper function.
   function dec2hex(dec) {
@@ -58,6 +71,7 @@ const library = (function () {
           synopsis
         );
       bookCollection[id] = details;
+      updateLocalStorage();
     },
 
     removeBook: function(lookup) {
@@ -66,6 +80,7 @@ const library = (function () {
             console.log(`deleting bookCollection item ${items[i]}`)
             delete bookCollection[items[i]];
         }
+        updateLocalStorage();
     },
 
     getAllBooks: function() {
@@ -100,21 +115,56 @@ Book.prototype.info = function () {
   return `${this.title}, ${this.author}, ${this.pages}, ${this.readStatus}`;
 };
 
+function contentCard(Book) {
+    const contentCard = document.createElement('div')
+    contentCard.classList.add('content-card');
+    
+    const template = `
+        <img class="cover-art" src=${Book.coverArt}>
+        <div class="content-details">
+            <span class="genre-label">
+                ${Book.genre}
+            </span>
+            <p class="title-label">
+                ${Book.title}
+            </p>
+            <span class="author-label">
+                ${Book.author}
+            </span>
+        </div>
 
+        <!-- Card Modal -->
+        <div class="modal-card">
+            <div class="modal__content">
+                <span class="close">&times;</span>
+                <h1>${Book.title}</h1>
+                <hr />
+                <h4>Author:${Book.author}</h3>
+                <h4>Page Count:${Book.pages}</h4>
+                <h4>I have read this book</h4>
+                <div class="synopsis">
+                    <h4>Synopsis:</h4>
+                    <p>${Book.synopsis}</p>
+                    <br>
+                    <div class="modal__buttons">
+                        <button class="btn btn-red">Delete Book</button>
+                    </div>
+                        
+                </div>
+            </div>
+        </div>
+    
+    `;
 
-/* library.addBook(
-  "The rise of Hyperion",
-  "Dan Simmons",
-  723,
-  "Science Fiction",
-  "25%",
-  "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTKbpGHJeFMd3UsEC5Cucn2U0E59U3nZmVBONonqEMYYIHm1Jt9",
-  "lt is the 29th century and the universe of the Human Hegemony is under threat. Invasion by the warlike Ousters looms, and the mysterious schemes of the secessionist AI TechnoCore bring chaos ever closer. On the eve of disaster, with the entire galaxy at war, seven pilgrims set fourth on a final voyage to the legendary Time Tombs on Hyperion, home to the Shrike, a lethal creature, part god and part killing machine, whose powers transcend the limits of time and space. The pilgrims have resolved to die before discovering anything less than the secrets of the universe itself."
-);
+    contentCard.innerHTML = template;
+    return contentCard;
+}
 
-library.addBook(
-    "Hyperion",
-    "Dan Simmons",
-    876,
-    "Science Fiction"
-) */
+let allBooks = library.getAllBooks();
+let page = document.getElementById('content-section');
+
+for(book in allBooks) {
+    let card = contentCard(allBooks[book]);
+    page.appendChild(card);
+
+}
